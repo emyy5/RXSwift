6f0caf2fff417033db0cb791c26604202e16816c
Github <emyy5>
2023-09-04 06:04:52 +0300

    First

--- RXSwiftLec1/ViewController.swift
+++ RXSwiftLec1/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmantControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
